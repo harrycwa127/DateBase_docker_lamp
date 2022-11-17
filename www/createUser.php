f420af2084172a475720f521e1e8a5b51758aaf3
hkhugo <74865305+hkhugo@users.noreply.github.com>
2022-11-17 17:06:35 +0800

    Revert "convert _log_error to alert"
    This reverts commit f260b9654b290fcd5cfb1c2cd730523bd488d981.

--- www/createUser.php
+++ www/createUser.php
@@ -2,10 +2,6 @@
 
 require_once '_route.php';
 
-function alert($msg) {
-    echo "<script type='text/javascript'>alert('$msg');</script>";
-}
-
 if (array_key_exists('is_logged', $_SESSION)) {
     header('Location: main.php');
     exit;
@@ -23,11 +19,11 @@ if ($_POST && isset($_POST['submit'])) {
 
         //Missing required fields
         if (_is_valid($username) === FALSE || _is_valid($password) === FALSE || _is_valid($passconf) === FALSE)
-            alert('Missing required fields');
+            _log_error('Missing required fields');
 
         //Passwords did not match
         if ($password != $passconf) {
-            alert('The passwords is not the same.');
+            _log_error('The passwords is not the same.');
         }
 
         //search username in db
@@ -39,7 +35,7 @@ if ($_POST && isset($_POST['submit'])) {
 
         //if same username found 
         if ($num > 0)
-            alert("Sorry, the username '{$username}' is already in use. <a href=\"createUser.php\">Go back</a>.</p>");
+            _log_error("Sorry, the username '{$username}' is already in use. <a href=\"createUser.php\">Go back</a>.</p>");
 
         //insert to db
         try {

0a51eaa34cfadab7dda638912b037c01b47f00f7
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2022-11-20 00:16:45 +0800

    change table display in index

---
+++ dump/myDb.sql
@@ -0,0 +1,110 @@
+SET time_zone = "+08:00";
+
+CREATE TABLE `Person` (
+  `id` int NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `age` int
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `Person` (`id`, `name`,`age`) VALUES
+(1, 'William',30),
+(2, 'Marc',17),
+(3, 'John',55);
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `user_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_name` VARCHAR(100) NOT NULL UNIQUE,
+    `user_password` VARCHAR(255) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
+
+-- Sample user and data
+
+INSERT INTO `users` (`user_id`, `user_name`,`user_password`) VALUES
+(1,'test','$2y$10$9phx9WxnXL2yDJ.Z5Ygaze5a9AiVSu1blMy/kBbh2og.1Z/rNCYx.'),
+(2,'User1','$2y$10$PARfzpp1B5HFPvC2gsr8cuIwTfZi6xuAEXTiJPHv7I2mqe0D1QhOe');
+
+
+
+-- Sample table and data
+
+-- Create bookorder table
+	
+CREATE TABLE bookorder(
+    Order_Number INT NOT NULL,
+    Cus_ID INT,
+    Mailing_Address VARCHAR(500),
+    Credit_Card_Number VARCHAR(16),
+    Shipment_Method VARCHAR(50),
+    Shipping_Date DATE,
+    Date_and_Time_of_Order DATETIME,
+    ISBN VARCHAR(17),
+    Price DECIMAL(7, 2),
+    Purchase_Price DECIMAL(7, 2),
+    Quantity_Purchased INT,
+    Shipping_Cost DECIMAL(7, 2),
+    Tax DECIMAL(7, 2),
+    PRIMARY KEY(Order_Number)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+INSERT INTO `bookorder` (`Order_Number`, `Cus_ID`, `Mailing_Address`, `Credit_Card_Number`, `Shipment_Method`, `Shipping_Date`, `Date_and_Time_of_Order`, `ISBN`, `Price`, `Purchase_Price`, `Quantity_Purchased`, `Shipping_Cost`, `Tax`) 
+VALUES (1, 1, 'John@gmail.com', '1111222233334444', 'ground', '2022-01-01', '2022-01-01 11:22:33', '1234567891234', 100, 120, 1, 30,5),
+(2, 3, 'Marc@gmail.com', '2222333344445555', 'ground', '2022-05-02', '2022-05-02 16:01:50', '5378295647392', 90, 110, 1, 30,5),
+(3, 3, 'William@gmail.com', '3333444455556666', 'ground', '2022-04-26', '2022-04-26 21:42:05', '7264820174832', 80, 100, 1, 30,4);
+
+
+-- Create customer table
+CREATE TABLE customer(
+    Fname VARCHAR(50),
+    MI VARCHAR(50),
+    Lname VARCHAR(50),
+    Cus_ID INT NOT NULL,
+    Address VARCHAR(500),
+    Credit_Card_Number VARCHAR(16),
+    Expiration_Date DATE,
+    PhoneNumber VARCHAR(20),
+    E_mail VARCHAR(50),
+    PRIMARY KEY(Cus_ID)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- Create book table
+CREATE TABLE book(
+    Title VARCHAR(200),
+    ISBN VARCHAR(17) NOT NULL,
+    PRIMARY KEY(ISBN),
+    Pub_ID INT,
+    Edition INT,
+    Date_of_Publication DATE,
+    Price DECIMAL(7, 2),
+    Book_Description VARCHAR(1000),
+    Quantity INT,
+    Category_Code INT
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- Create publisher table
+CREATE TABLE publisher(
+    NAME VARCHAR(100),
+    Pub_ID INT NOT NULL,
+    Address VARCHAR(500),
+    PRIMARY KEY(Pub_ID)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- Create category table
+CREATE TABLE category(
+    Category_Code INT NOT NULL,
+    Category_Description VARCHAR(1000),
+    Sub_Category_Code INT,
+    PRIMARY KEY(Category_Code)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- Create author table
+CREATE TABLE author(
+    Fname VARCHAR(50) NOT NULL,
+    MI VARCHAR(50) NOT NULL,
+    Lname VARCHAR(50) NOT NULL,
+    ISBN VARCHAR(17) NOT NULL,
+    Title VARCHAR(200)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+
+-- user privilege
+
--- www/index.php
+++ www/index.php
@@ -28,17 +28,20 @@ if (array_key_exists('is_logged', $_SESSION) === FALSE) {
             <a href="logout.php">Logout</a>
         </td>
 
-        <?php echo "<h1>Users</h1>"; ?>
+        <?php echo "<h1>Bookorder</h1>"; ?>
         <?php
         require_once '_route.php';
-        $query = 'SELECT * From Person';
+        $query = 'SELECT * From bookorder';
         $result = mysqli_query($conn, $query);
 
         echo '<table class="table table-striped">';
-        echo '<thead><tr><th></th><th>id</th><th>name</th><th>age</th></tr></thead>';
+        echo '<thead><tr><th></th><th>Order Number</th><th>Customer ID</th><th>Mail Address</th><th>Shipment Method</th><th>Shipping Date</th><th>Date and Time of Order</th><th>ISBN</th><th>Price</th><th>Purchase Price</th><th>Quantity Purchased</th><th>Shipping Cost</th><th>Tax</th></tr></thead>';
         while ($value = $result->fetch_array(MYSQLI_ASSOC)) {
             echo '<tr>';
             echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+
+            // unset credit_card information
+            unset($value[3]);
             foreach ($value as $element) {
                 echo '<td>' . $element . '</td>';
             }

d914056e7876465b95bd7460c5aa7f5e70fe118b
hkhugo <74865305+hkhugo@users.noreply.github.com>
2022-11-20 12:23:59 +0800

    Update insert php layout design

--- www/insert.php
+++ www/insert.php
@@ -1,77 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <title>Add Data</title>
-   </head>
-   <body>
-      
-         <h1>Add Data into the table</h1>
-         <form action="insertTodb.php" method="post">
-            <p>
-               <label for="CusID">Cus ID:</label>
-               <input type="text" name="Cus_ID" id="CusID">
-            </p> 
-            
-            <p>
-               <label for="MailingAddress">Mailing Address:</label>
-               <input type="email" name="Mailing_Address" id="MailingAddress">
-            </p>
-            
-            <p>
-               <label for="CreditCardNumber">Credit Card Number:</label>
-               <input type="text" name="Credit_Card_Number" id="CreditCardNumber">
-            </p>
-            
-            <p>
-               <label for="ShipmentMethod">Shipment Method:</label>
-               <input type="text" name="Shipment_Method" id="ShipmentMethod">
-            </p>
 
-            <p>
-               <label for="ShippingDate">Shipping Date:</label>
-               <input type="text" name="Shipping_Date" id="ShippingDate">
-            </p>
+<head>
+   <title>Add Data</title>
+</head>
 
-            <p>
-               <label for="DateandTimeofOrder">Date and Time of Order:</label>
-               <input type="datetime-local" name="Date_and_Time_of_Order" id="DateandTimeofOrder">
-            </p>
+<body>
 
-            <p>
-               <label for="ISBN">ISBN:</label>
-               <input type="text" name="ISBN" id="ISBN">
-            </p>
+   <h1>Add Data into the table</h1>
+   <form action="insertTodb.php" method="post">
+      <table border="0">
+         <tr>
+            <td><label for="CusID">Cus ID:</label></td>
+            <td><input type="text" name="Cus_ID" id="CusID"></td>
+         </tr>
 
-            <p>
-               <label for="Price">Price:</label>
-               <input type="text" name="Price" id="Price">
-            </p>
+         <tr>
+            <td><label for="MailingAddress">Mailing Address:</label></td>
+            <td><input type="email" name="Mailing_Address" id="MailingAddress"></td>
+         </tr>
 
-            <p>
-               <label for="PurchasePrice">Purchase Price:</label>
-               <input type="text" name="Purchase_Price" id="PurchasePrice">
-            </p>
+         <tr>
+            <td><label for="CreditCardNumber">Credit Card Number:</label></td>
+            <td><input type="text" name="Credit_Card_Number" id="CreditCardNumber"></td>
+         </tr>
 
-            <p>
-               <label for="QuantityPurchased">Quantity Purchased:</label>
-               <input type="text" name="Quantity_Purchased" id="QuantityPurchased">
-            </p>
+         <tr>
+            <td><label for="ShipmentMethod">Shipment Method:</label></td>
+            <td><input type="text" name="Shipment_Method" id="ShipmentMethod"></td>
+         </tr>
 
-            <p>
-               <label for="ShippingCost">Shipping Cost:</label>
-               <input type="text" name="Shipping_Cost" id="ShippingCost">
-            </p>
+         <tr>
+            <td><label for="ShippingDate">Shipping Date:</label></td>
+            <td><input type="text" name="Shipping_Date" id="ShippingDate"></td>
+         </tr>
 
-            <p>
-               <label for="Tax">Tax:</label>
-               <input type="text" name="Tax" id="Tax">
-            </p>
+         <tr>
+            <td><label for="DateandTimeofOrder">Date and Time of Order:</label></td>
+            <td><input type="datetime-local" name="Date_and_Time_of_Order" id="DateandTimeofOrder"></td>
+         </tr>
 
-            <input type="reset">
+         <tr>
+            <td><label for="ISBN">ISBN:</label></td>
+            <td><input type="text" name="ISBN" id="ISBN"></td>
+         </tr>
 
-            <input type="submit" value="Submit">
-         
-         </form>
-      
-   </body>
+         <tr>
+            <td><label for="Price">Price:</label></td>
+            <td><input type="text" name="Price" id="Price"></td>
+         </tr>
+
+         <tr>
+            <td><label for="PurchasePrice">Purchase Price:</label></td>
+            <td><input type="text" name="Purchase_Price" id="PurchasePrice"></td>
+         </tr>
+
+         <tr>
+            <td><label for="QuantityPurchased">Quantity Purchased:</label></td>
+            <td><input type="text" name="Quantity_Purchased" id="QuantityPurchased"></td>
+         </tr>
+
+         <tr>
+            <td><label for="ShippingCost">Shipping Cost:</label></td>
+            <td><input type="text" name="Shipping_Cost" id="ShippingCost"></td>
+         </tr>
+
+         <tr>
+            <td><label for="Tax">Tax:</label></td>
+            <td><input type="text" name="Tax" id="Tax"></td>
+         </tr>
+      </table>
+      <input type="reset">
+      <input type="submit" value="Submit">
+   </form>
+</body>
+
 </html>

fec444dc2b95fdbcfb1b07e3d34c59938b8ee97a
hkhugo <74865305+hkhugo@users.noreply.github.com>
2022-11-20 14:53:32 +0800

    Fix bug in insert data

--- www/insertTodb.php
+++ www/insertTodb.php
@@ -16,9 +16,11 @@
     try {
         $insert_db = new PDO(
             "mysql:host={$curd_dbhost};dbname={$curd_dbname};",
-            $curd_dbuser, $curd_dbpassword);
+            $curd_dbuser,
+            $curd_dbpassword
+        );
         $insert_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(Exception $e) {
+    } catch (Exception $e) {
         error_log('Connection failed: ' . $e->getMessage());
     }
     //$crud_conn = mysqli_connect($curd_dbhost, $curd_dbuser, $curd_dbpassword, $curd_dbname);
@@ -46,7 +48,8 @@
     $Credit_Card_Number = trim(filter_input(INPUT_POST, 'Credit_Card_Number', FILTER_SANITIZE_STRING));
     $Shipment_Method = trim(filter_input(INPUT_POST, 'Shipment_Method', FILTER_SANITIZE_STRING));
     $Shipping_Date = trim(filter_input(INPUT_POST, 'Shipping_Date', FILTER_SANITIZE_STRING));
-    $Date_and_Time_of_Order = $_REQUEST['Date_and_Time_of_Order'];
+    $date = $_REQUEST['Date_and_Time_of_Order'];
+    $Date_and_Time_of_Order = date('Y-m-d H:i:s', strtotime($date));
     $ISBN = trim(filter_input(INPUT_POST, 'ISBN', FILTER_SANITIZE_STRING));
     $Price = trim(filter_input(INPUT_POST, 'Price', FILTER_SANITIZE_STRING));
     $Purchase_Price = trim(filter_input(INPUT_POST, 'Purchase_Price', FILTER_SANITIZE_STRING));
@@ -55,14 +58,20 @@
     $Tax = trim(filter_input(INPUT_POST, 'Tax', FILTER_SANITIZE_STRING));
 
 
-    // Performing insert query execution
+    //test --> Performing insert query execution
     // $sql = "INSERT INTO bookorder ('Cus_ID', 'Mailing_Address', 'Credit_Card_Number', 'Shipment_Method', 'Shipping_Date', 'Date_and_Time_of_Order', 'ISBN', 'Price', 'Purchase_Price', 'Quantity_Purchased', 'Shipping_Cost', 
     // 'Tax') VALUES ('". $_REQUEST['Cus_ID'] ."','". $_REQUEST['Mailing_Address'] ."','". $_REQUEST['Credit_Card_Number'] ."','". $_REQUEST['Shipment_Method'] ."','". $_REQUEST['Shipping_Date'] ."',
     // '". $_REQUEST['Date_and_Time_of_Order'] ."','". $_REQUEST['ISBN'] ."','". $_REQUEST['Price'] ."','". $_REQUEST['Purchase_Price'] ."','". $_REQUEST['Quantity_Purchased'] ."','". $_REQUEST['Shipping_Cost'] ."','". $_REQUEST['Tax'] ."')";
 
-    $sql = "INSERT INTO bookorder  ('$Cus_ID','$Mailing_Address','$Credit_Card_Number','$Shipment_Method','$Shipping_Date',
-        '$Date_and_Time_of_Order','$ISBN','$Price','$Purchase_Price','$Quantity_Purchased','$Shipping_Cost','$Tax')";
-    
+    // $sql = "INSERT INTO bookorder  ('$Cus_ID','$Mailing_Address','$Credit_Card_Number','$Shipment_Method','$Shipping_Date',
+    //     '$Date_and_Time_of_Order','$ISBN','$Price','$Purchase_Price','$Quantity_Purchased','$Shipping_Cost','$Tax')";
+
+    //Performing insert query execution
+    $sql = "INSERT INTO `bookorder` (`Cus_ID`, `Mailing_Address`, `Credit_Card_Number`, `Shipment_Method`, `Shipping_Date`, 
+    `Date_and_Time_of_Order`, `ISBN`, `Price`, `Purchase_Price`, `Quantity_Purchased`, `Shipping_Cost`, `Tax`) 
+    VALUES ('$Cus_ID','$Mailing_Address','$Credit_Card_Number','$Shipment_Method','$Shipping_Date',
+            '$Date_and_Time_of_Order','$ISBN','$Price','$Purchase_Price','$Quantity_Purchased','$Shipping_Cost','$Tax')";
+
     try {
         $stmt = $insert_db->prepare($sql);
         $stmt->execute();
@@ -74,12 +83,12 @@
         _log_error('Error: ' . $e->getMessage());
     }
 
-    // if (mysqli_query($crud_conn, $sql)) {
-        echo "<h3>data stored in a database successfully."
-            . " Please browse your localhost php my admin"
-            . " to view the updated data</h3>";
+    // upload success and result message
+    echo "<h3>data stored in a database successfully."
+        . " Please browse your localhost php my admin"
+        . " to view the updated data</h3>";
 
-        echo nl2br("\n$Order_Number\n$Cus_ID\n$Mailing_Address\n$Credit_Card_Number\n$Shipment_Method\n$Shipping_Date\n
+    echo nl2br("\n$Order_Number\n$Cus_ID\n$Mailing_Address\n$Credit_Card_Number\n$Shipment_Method\n$Shipping_Date\n
             $Date_and_Time_of_Order\n$ISBN\n$Price\n$Purchase_Price\n$Quantity_Purchased\n$Shipping_Cost\n$Tax");
     // } else {
     //     _log_error ("ERROR: Hush! Sorry $sql. "
